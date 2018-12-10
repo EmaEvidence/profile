097fcc59b881624f60a93d5ea8f18ef43d855572
EmaEvidence <emmanuelalabi563@gmail.com>
2018-12-10 16:58:33 +0100

    Updates

---
+++ precache-manifest.52f06283b7e1b2993ba942ae6a31b61a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "31ef00bbcda6fddb16e3120279975b64",
+    "url": "/profile/static/media/ema.31ef00bb.png"
+  },
+  {
+    "revision": "6901c65d6d291c5e2cbb9b44aaa905f7",
+    "url": "/profile/static/media/FuturaTEERegular.6901c65d.ttf"
+  },
+  {
+    "revision": "0dae820b9a75200b92d22deed0c7cc26",
+    "url": "/profile/static/media/ForgottenFuturistCollegeRegular.0dae820b.ttf"
+  },
+  {
+    "revision": "92700b96a6acba4945f6",
+    "url": "/profile/static/js/runtime~main.92700b96.js"
+  },
+  {
+    "revision": "f02b5cf87907980d60cf",
+    "url": "/profile/static/js/main.f02b5cf8.chunk.js"
+  },
+  {
+    "revision": "2de967611da4c3ea74cb",
+    "url": "/profile/static/js/1.2de96761.chunk.js"
+  },
+  {
+    "revision": "f02b5cf87907980d60cf",
+    "url": "/profile/static/css/main.940a03bd.chunk.css"
+  },
+  {
+    "revision": "aedcfded90b3737c368dd4455676c4af",
+    "url": "/profile/index.html"
+  }
+];
