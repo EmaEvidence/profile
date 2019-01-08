d5d8dbc131b80b9be3e06b4249623debe7f0ab0a
EmaEvidence <emmanuelalabi563@gmail.com>
2019-01-08 17:02:42 +0100

    add connect page

---
+++ build/precache-manifest.b749a1b57a57fd1e842a4ebee7031ed8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "31ef00bbcda6fddb16e3120279975b64",
+    "url": "/profile/static/media/ema.31ef00bb.png"
+  },
+  {
+    "revision": "6901c65d6d291c5e2cbb9b44aaa905f7",
+    "url": "/profile/static/media/FuturaTEERegular.6901c65d.ttf"
+  },
+  {
+    "revision": "0dae820b9a75200b92d22deed0c7cc26",
+    "url": "/profile/static/media/ForgottenFuturistCollegeRegular.0dae820b.ttf"
+  },
+  {
+    "revision": "92700b96a6acba4945f6",
+    "url": "/profile/static/js/runtime~main.92700b96.js"
+  },
+  {
+    "revision": "8de9dda2ea74f0457635",
+    "url": "/profile/static/js/main.8de9dda2.chunk.js"
+  },
+  {
+    "revision": "0b56793c97c1d8486016",
+    "url": "/profile/static/js/1.0b56793c.chunk.js"
+  },
+  {
+    "revision": "8de9dda2ea74f0457635",
+    "url": "/profile/static/css/main.52a4170d.chunk.css"
+  },
+  {
+    "revision": "5e8e8b048cf89a858cab523055e4cf0b",
+    "url": "/profile/index.html"
+  }
+];
